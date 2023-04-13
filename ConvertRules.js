5d6c187e6f04d57ba6aee530925850ce2692c67a
Patrick Hall <pathall@gmail.com>
2023-04-13 12:53:09 -0400

    first commit

---
+++ ConvertRules.js
@@ -0,0 +1,67 @@
+class ConvertRules extends HTMLElement {
+  constructor(){
+    super()
+
+    this.innerHTML = `
+  <textarea class=rules></textarea>
+  <textarea class=ephemeral></textarea>
+  <textarea class=output></textarea>
+    `
+    this.listen()
+  }
+
+  applyRules(s){
+
+    this.rules.forEach(([before,after]) => {
+      s = s.replaceAll(before, after)
+    })
+    
+    return s
+  }
+
+  async fetch(url){
+    let response = await fetch(url)
+    let data = await response.json()
+    this.data = data
+  }
+
+  connectedCallback(){
+
+  }
+
+  get rules(){
+    let rawRules = this.querySelector('textarea.rules').value
+
+    return rawRules.trim().split('\n')
+      .map(line => line.trim()
+        .split("\t")
+        .map(line => line.trim())
+      )
+  }
+
+  static get observedAttributes(){
+    return ["src"]
+  }
+
+  attributeChangedCallback(attribute, oldValue, newValue){
+    if(attribute == "src"){
+      this.fetch(newValue)
+    }
+  }
+
+  render(){
+    // edit .innerHTML here
+  }
+
+  listen(){
+    this.addEventListener('keyup', pasteEvent => {
+      let emphemeralString = this.querySelector('textarea.ephemeral').value
+      let unicodeString = this.applyRules(emphemeralString)
+
+      this.querySelector('textarea.output').value = unicodeString
+    })
+  }
+}
+
+export {ConvertRules}
+customElements.define('convert-rules', ConvertRules)
---
+++ convert-rules-docs.md
@@ -0,0 +1,61 @@
+---
+lang: en
+title:  \<convert-rules\> docs
+css: convert-rules.css
+---
+
+<main>
+
+<section id=example>
+## Example
+
+
+```html
+<convert-rules></convert-rules>
+```
+
+```{=html}
+<convert-rules></convert-rules>
+```
+
+
+
+</section>
+
+<section id=attributes>
+## Attributes
+
+</section>
+
+<section id=methods>
+## Methods
+
+</section>
+
+<section id=data>
+## Data
+
+</section>
+
+<section id=events>
+## Events
+
+</section>
+
+<section id=layouts>
+## Layouts
+
+</section>
+
+<section id=see-also>
+## See also
+
+</main>
+
+
+<script type="module">
+import {ConvertRules} from './ConvertRules.js'
+
+window.convertRules = document.querySelector('convert-rules')
+</script>
+
